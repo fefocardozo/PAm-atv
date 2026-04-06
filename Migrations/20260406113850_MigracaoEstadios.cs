4e1db96dad7fa2074857da23c43613e016a2722c
Gustavoh345 <felipesardinham@gmail.com>
2026-04-06 09:15:54 -0300

    atividade

---
+++ Migrations/20260406113850_MigracaoEstadios.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CopaHAS.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoEstadios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_ESTADIOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true),
+                    Cidade = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true),
+                    Capacidade = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_ESTADIOS", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TB_ESTADIOS",
+                columns: new[] { "Id", "Capacidade", "Cidade", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "78838", "Rio de Janeiro", "Maracaña" },
+                    { 2, "49205", "São Paulo", "Neo Química Arena" },
+                    { 3, "61842", "Belo Horizonte", "Mineirão" },
+                    { 4, "50842", "Porto Alegre", "Beira Rio" },
+                    { 5, "50000", "Salvador", "Arena Fonte Nova" },
+                    { 6, "44000", "Manaus", "Arena da Amazônia" },
+                    { 7, "72788", "Brasília", "Mané Garrincha" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_ESTADIOS");
+        }
+    }
+}
---
+++ script02_TabelaEstadios.sql
@@ -0,0 +1,28 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE [TB_ESTADIOS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] varchar(200) NULL,
+    [Cidade] varchar(200) NULL,
+    [Capacidade] varchar(200) NULL,
+    CONSTRAINT [PK_TB_ESTADIOS] PRIMARY KEY ([Id])
+);
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Capacidade', N'Cidade', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ESTADIOS]'))
+    SET IDENTITY_INSERT [TB_ESTADIOS] ON;
+INSERT INTO [TB_ESTADIOS] ([Id], [Capacidade], [Cidade], [Nome])
+VALUES (1, '78838', 'Rio de Janeiro', 'Maracaña'),
+(2, '49205', 'São Paulo', 'Neo Química Arena'),
+(3, '61842', 'Belo Horizonte', 'Mineirão'),
+(4, '50842', 'Porto Alegre', 'Beira Rio'),
+(5, '50000', 'Salvador', 'Arena Fonte Nova'),
+(6, '44000', 'Manaus', 'Arena da Amazônia'),
+(7, '72788', 'Brasília', 'Mané Garrincha');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Capacidade', N'Cidade', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ESTADIOS]'))
+    SET IDENTITY_INSERT [TB_ESTADIOS] OFF;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260406113850_MigracaoEstadios', N'10.0.5');
+
+COMMIT;
+GO
+
